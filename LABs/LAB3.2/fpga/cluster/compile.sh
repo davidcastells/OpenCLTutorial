59c6ebbe93633da192e0c41a8b64718d926c6078
David Castells-Rufas <david.castells@uab.cat>
2020-02-05 17:31:17 +0100

    cluster

---
+++ LABs/LAB3.2/fpga/cluster/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=lab3.2
+#SBATCH -N 1 # number of nodes
+#SBATCH -c 2 # number of cores
+#SBATCH -p nebula.q
+
+module load intelfpga-opencl/17.1
+
+time aoc median.cl
---
+++ LABs/LAB3.2/fpga/cluster/submit.sh
@@ -0,0 +1,2 @@
+cd LAB3.2
+/soft/slurm-18.08.4/bin/sbatch compile.sh
